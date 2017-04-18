cf66ccf052c001469494762041796f3396dacd02
Manjunatha S V <manjunath@qreat.in>
2017-04-18 14:41:41 +0530

    refactor

--- src/models/user.js
+++ src/models/user.js
@@ -5,69 +5,69 @@ const RoleModel = require('./role.js');
 const q = require('q');
 
 class User extends Model{
-	constructor(){
-		super();
-		this.collection = 'users';
-		this.schema = {
-			properties :{
-				id: {
-					type: 'string',
-					required: true
-				},
-				roles: {
-					type: 'array',
-					required: true,
-					default: ['user']
-				}
-			}
-		}
-	}
-	/*method which returns permissions associated with a user*/
+  constructor(){
+    super();
+    this.collection = 'users';
+    this.schema = {
+      properties :{
+        id: {
+          type: 'string',
+          required: true
+        },
+        roles: {
+          type: 'array',
+          required: true,
+          default: ['user']
+        }
+      }
+    }
+  }
+  /*method which returns permissions associated with a user*/
   getPermissions(userId){
-  	var defered = q.defer();
-  	this.getRoles(userId).then((roles)=>{
-  		this.getPermissionsList(roles).then((permList)=>{
-  			var permArray = Array.from(permList);
-  			var permModel = new PermissionModel();
-  			this.getPermissionListByIds(permArray).then((result)=>{
-  				defered.resolve(result);
-  			})
-  		})
-  	});
-  	return defered.promise;
+    var defered = q.defer();
+    this.getRoles(userId).then((roles)=>{
+      this.getPermissionsList(roles).then((permList)=>{
+        var permArray = Array.from(permList);
+        var permModel = new PermissionModel();
+        this.getPermissionListByIds(permArray).then((result)=>{
+          defered.resolve(result);
+        })
+      })
+    });
+    return defered.promise;
   }
   /*method to check if a given user and permission pair is valid*/
   checkPermission(userperm){
-  	var defered = q.defer();
-  	this.getPermissions(userperm.userId).then((permissions)=>{
-  		var present = false;
-  		var tasksTogo = permissions.length;
-  		permissions.forEach((element)=>{
-  			if(element.id == userperm.permId){
-  				present = true;
-  				defered.resolve(present);
-  			}
-  			if(--tasksTogo == 0){
-		    	defered.resolve(present);
-		  	}
-  		});
-  	})
+    var defered = q.defer();
+    this.getPermissions(userperm.userId).then((permissions)=>{
+      var present = false;
+      var tasksTogo = permissions.length;
+      permissions.forEach((element)=>{
+        if(element.id == userperm.permId){
+          present = true;
+          defered.resolve(present);
+        }
+        if(--tasksTogo == 0){
+          defered.resolve(present);
+        }
+      });
+    })
   return defered.promise;
   }
   /*method which returns roles of a particular user based on id*/
-	getRoles(userId){
+  getRoles(userId){
     var defered = q.defer();
     var filterObj = {};
     filterObj['id'] = userId;
-	    super.read(filterObj).then(function(docs){
-	      defered.resolve(docs[0].roles);
-	    }).fail(function(err){
-	      defered.reject(err);
-	    })
+      super.read(filterObj).then(function(docs){
+        defered.resolve(docs[0].roles);
+      }).fail(function(err){
+        defered.reject(err);
+      })
     return defered.promise;
   }
   /*A helper method which returns a union of permissions associated with a set of roles*/
-	getPermissionsList(roles){
+  getPermissionsList(roles){
     var defered = q.defer();
     var tasksTogo = roles.length;
     var myPermissions = new Set();
@@ -75,19 +75,19 @@ class User extends Model{
       var filterObj = {};
       var roleModel = new RoleModel();
       filterObj['id'] = element;
-	    	roleModel.read(filterObj).then(function(docs){
-		      var permArr = docs[0].permissions;
-		      permArr.forEach(function(element){
-		        myPermissions.add(element);
-		      })
-		      if(--tasksTogo == 0){
-		        defered.resolve(myPermissions);
-		      }
-	      }).fail(function(err){
-	          defered.reject(err);
-	      });
+        roleModel.read(filterObj).then(function(docs){
+          var permArr = docs[0].permissions;
+          permArr.forEach(function(element){
+            myPermissions.add(element);
+          })
+          if(--tasksTogo == 0){
+            defered.resolve(myPermissions);
+          }
+        }).fail(function(err){
+            defered.reject(err);
+        });
       })
-		return defered.promise;
+    return defered.promise;
   }
 
   /*A helpere method which returns an array of permission objects based on permission-id*/
@@ -99,14 +99,14 @@ class User extends Model{
       var filterObj = {};
       filterObj['id'] = element;
       var permModel = new PermissionModel();
-	      permModel.read(filterObj).then(function(permissions){
-	        myPermList.push(permissions[0]);
-	      if(--tasksToGo == 0){
-	        defered.resolve(myPermList);
-	      }
-	      }); 
+        permModel.read(filterObj).then(function(permissions){
+          myPermList.push(permissions[0]);
+        if(--tasksToGo == 0){
+          defered.resolve(myPermList);
+        }
+        }); 
     })
     return defered.promise;
-	} 
+  } 
 }
 module.exports = User;
